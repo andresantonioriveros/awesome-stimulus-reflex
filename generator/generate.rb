19420b0c6b7d8597008968ee42cc1e6fa569f15f
Andrew Mason <andrewmcodes@protonmail.com>
2020-05-09 06:34:35 -0400

    try action

--- generator/generate.rb
+++
@@ -1,5 +0,0 @@
-require "liquid"
-require "yaml"
-data = YAML.safe_load(File.read("../data.yml"))
-output = Liquid::Template.parse(File.read('./template.liquid')).render!('data' => data)
-File.write('../README.md', output)
